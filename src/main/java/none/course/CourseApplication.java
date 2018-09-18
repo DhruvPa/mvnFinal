1abe29920b176b2d04c87ba0bcbb11bd5b8b576f
Dhruv Pathak <dhruv.pathak@hsc.com>
2018-09-18 14:56:54 +0530

    changes

--- src/main/java/none/course/CourseApplication.java
+++ src/main/java/none/course/CourseApplication.java
@@ -2,11 +2,19 @@ package none.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CourseApplication {
+public class CourseApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CourseApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CourseApplication.class, args);
 	}
+
 }
